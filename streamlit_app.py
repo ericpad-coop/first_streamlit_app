98a2e9087701c1d744fae6d20ed0396a47e7ed15
ericpad-coop <148784372+ericpad-coop@users.noreply.github.com>
2023-10-23 16:37:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-streamlit.text("** Begin Snowflake test **)
+streamlit.text('** Begin Snowflake test **')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -49,6 +49,6 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-streamlit.text("** End Snowflake test **)
+streamlit.text('** End Snowflake test **')
 
 
