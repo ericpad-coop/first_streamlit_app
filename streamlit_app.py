f4a9294e940882eb99bed36c1e912ebf3916bb24
ericpad-coop <148784372+ericpad-coop@users.noreply.github.com>
2023-10-23 10:28:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 # main python file
 import streamlit
 
-streamlit.title("My Parents new healthy diner")
+streamlit.title('My Parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberrry Oatmeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hardboiled Free Range Egg')
+
