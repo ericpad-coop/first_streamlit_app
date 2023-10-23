244701f52e5a0949cc2868ff350a2879f3ccbf3b
ericpad-coop <148784372+ericpad-coop@users.noreply.github.com>
2023-10-23 16:50:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header('My fruit load list contains:')
 streamlit.dataframe(my_data_rows)
 
+add_fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 
 streamlit.text('** End Snowflake test **')
 
